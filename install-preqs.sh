dd1716f93bb40ee04ed2968f0d5bce8d4e9b73fc
Don Perry <don.perry@dokainc.com>
2017-01-20 11:23:37 -0600

    Code cleanup

--- install-preqs.sh
+++ install-preqs.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
-#LINUX_VERSION=$(grep ^NAME /etc/*ease | sed 's/NAME=//g')
 
-    if [ -f /usr/bin/dnf ]
-    then
-        sudo dnf update
-        sudo dnf install python python-pip wget unzip npm nodejs nodejs-legacy -y
-    elif [ -f /usr/bin/yum ]
-    then
-        sudo yum update
-        sudo yum install python python-pip wget unzip npm nodejs nodejs-legacy -y
-    elif [ -f /usr/bin/apt-get ]
-    then
-        sudo apt-get update -y
-        sudo apt-get upgrade -y
-        sudo apt-get install python python-pip wget unzip npm nodejs nodejs-legacy -y
-    else
-        echo "Unsupported Linux version.  Neither yum, dnf or apt-get is installed"
-        exit 1
-    fi
+if [ -f /usr/bin/dnf ]
+then
+    sudo dnf update -y
+    sudo dnf install python python-pip wget unzip nodejs -y
+else
+    echo "dnf is not installed.  You should be using Fedora 25 which uses dnf for package installs."
+    exit 1
+fi
+
 
 # Check for ssh keys and generate if necessary
 if [ -f ~/.ssh/id_rsa.pub ]
