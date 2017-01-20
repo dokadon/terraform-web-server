44905f8950d71c5885f20b0989a80f8a6e33d5d6
Don Perry <don.perry@dokainc.com>
2017-01-19 23:45:24 -0600

    Lots of bug fixes and enhancements

--- install-preqs.sh
+++ install-preqs.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env bash
 
-wget -nc https://releases.hashicorp.com/terraform/0.8.4/terraform_0.8.4_linux_amd64.zip
+LINUX_VERSION=$(grep ^NAME /etc/*ease | sed 's/NAME=//g')
+
+if [ LINUX_VERSION = 'Fedora' ]
+then
+    if [ -f /usr/bin/dnf ]
+    then
+        sudo dnf update
+        sudo dnf install python python-pip wget unzip nodejs -y
+    else if [ -f /usr/bin/yum ]
+        sudo yum update
+        sudo yum install python python-pip wget unzip nodejs -y
+    else
+        echo "You're running a RHEL based OS and do not have yum, or dnf installed"
+        exit(1)
+    fi
+else if [ LINUX_VERSION = 'Ubuntu' ]
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get install python python-pip wget unzip nodejs -y
+else
+    echo "You're running neither a RHEL nor Ubuntu based OS and are therefore not supported"
+    exit(1)
+fi
 
 # Check for ssh keys and generate if necessary
-if [ -r ~/.ssh/id_rsa.pub ]
+if [ -f ~/.ssh/id_rsa.pub ]
 then
     echo "Using existing ssh keys"
 else
@@ -12,17 +34,19 @@ else
 fi
 
 # Create /usr/local/bin if necessary
-if [ -r "/usr/local/bin/terraform" ]
+if [ -f "/usr/local/bin/terraform" ]
 then
     echo "/usr/local/bin/terraform exists"
 else
     echo "Installing terraform"
-    mkdir -p /usr/local/bin
-    unzip terraform_0.8.4_linux_amd64.zip -d /usr/local/bin
+    wget -nc https://releases.hashicorp.com/terraform/0.8.4/terraform_0.8.4_linux_amd64.zip
+    sudo mkdir -p /usr/local/bin
+    sudo unzip terraform_0.8.4_linux_amd64.zip -d /usr/local/bin
 fi
 
-pip install -U pytest
-pip install selenium
-pip install pytest-selenium
-pip install nodejs
-npm -g install phantomjs-prebuilt
+sudo pip install --upgrade pip
+sudo pip install awscli
+sudo pip install -U pytest
+sudo pip install selenium
+sudo pip install pytest-selenium
+sudo npm -g install phantomjs-prebuilt
