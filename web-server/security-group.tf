fddc2c505a2b1ebd1b7a1323d564e8c210049129
Don Perry <don.perry@dokasystems.com>
2019-08-30 00:57:29 -0500

    Create security-group.tf
    Security group refactored into separate file.

---
+++ web-server/security-group.tf
@@ -0,0 +1,25 @@
+# Create security group with web and ssh access
+resource "aws_security_group" "web_server" {
+  name = "web_server"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
