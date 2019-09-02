b0107882536c1912c2dfee27d80cd103181d1b24
Don Perry <don.perry@dokasystems.com>
2019-09-02 08:52:53 -0500

    Update test_web_server.py
    Change to firefox headless using geckodriver instead of obsolete/deprecated PhantomJS using Nodejs.
    Add test for Jenkins de[;oyment

--- web-server-jenkins.puppet/test_web_server.py
+++ web-server-jenkins.puppet/test_web_server.py
@@ -1,12 +1,16 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 class WebServerVerify(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.PhantomJS()
+        options = Options()
+        options.headless= True
+        self.driver = webdriver.Firefox(options=options)
         f = open('public-ip.txt', 'r')
         public_ip = f.read()
         self.host = "http://%s" % public_ip
+        self.jenkins_host = "%s:8080" % self.host        
 
     def testTitle(self):
         # Get web page
@@ -21,8 +25,15 @@ class WebServerVerify(unittest.TestCase):
 
         # Verify page heading
         heading=self.driver.find_element_by_tag_name('h1')
-        self.assertIn('Automation for the People', heading.text)
+        self.assertIn('Test Web Server', heading.text)
+        
+    def testJenkins(self):
+        # Get web page
+        self.driver.get(self.jenkins_host)
 
+        # Verify page title
+        self.assertIn('Sign in [Jenkins]', self.driver.title)       
+        
     def tearDown(self):
         self.driver.quit()
 
