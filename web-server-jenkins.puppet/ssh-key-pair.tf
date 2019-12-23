1cc4c9dde66c5770ccd746db74b81f5a65ac4fe2
Don Perry <don.perry@dokasystems.com>
2019-12-23 17:57:44 -0600

    Update ssh-key-pair.tf
    Hardcoded key name changed to variable

--- web-server-jenkins.puppet/ssh-key-pair.tf
+++ web-server-jenkins.puppet/ssh-key-pair.tf
@@ -1,5 +1,6 @@
 # Deploy ssh key for instance access
 resource "aws_key_pair" "deployer" {
-  key_name = "web_server" 
+  key_name   = "${var.ssh_key_name}"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
+
