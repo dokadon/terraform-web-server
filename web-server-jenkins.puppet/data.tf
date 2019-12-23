832f18f2b3b84ec9c67e1eaf09e19220699e3e62
Don Perry <don.perry@dokasystems.com>
2019-12-23 17:54:48 -0600

    Update data.tf
    Hard coded ami value changed to variable

--- web-server-jenkins.puppet/data.tf
+++ web-server-jenkins.puppet/data.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["${var.ami}"]
   }
   filter {
     name   = "virtualization-type"
