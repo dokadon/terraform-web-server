c2b369006be965680e686040eb9355044597bef4
Don Perry <don.perry@dokasystems.com>
2019-12-23 17:48:19 -0600

    Update main.tf
    1) Upgraded to ubuntu 19.04
    2) Uses userdata instead of remote-exec provisioner

--- web-server-jenkins.puppet/main.tf
+++ web-server-jenkins.puppet/main.tf
@@ -1,60 +1,36 @@
-# Provision a web server instance using the latest Ubuntu 16.04 on a
-# t2.micro node with an AWS Tag naming it "web-server"
-provider "aws" {
-    region = "us-east-2"
-}
+# Provision a web server instance using the latest Ubuntu 19.04 on a
+# t2.small node with an AWS Tag naming it "jenkins"
 
 # Create web server
-resource "aws_instance" "web_server" {
-    ami = "${data.aws_ami.ubuntu.id}"
-    vpc_security_group_ids = ["${aws_security_group.web_server.id}"]
-    instance_type = "t2.micro"
-    key_name      = "web_server"
-    tags = {
-        Name = "web-server"
-    }
-
-  connection {
-    user         = "ubuntu"
-    host         = "${self.public_ip}"
-    private_key  = "${file("~/.ssh/id_rsa")}"
+resource "aws_instance" "jenkins" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  vpc_security_group_ids = ["${aws_security_group.jenkins.id}"]
+  instance_type          = "t2.small"
+  key_name               = "${var.ssh_key_name}"
+  user_data              = "${file("userdata.sh")}"
+  tags = {
+    Name = "${var.server_name}"
   }
 
-  provisioner "file" {
-    source = "jenkins.pp"
-    destination = "/home/ubuntu/jenkins.pp"
+  ebs_block_device {
+    device_name = "/dev/sdh"
+    volume_size = 10
   }
-    
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo wget https://apt.puppetlabs.com/puppet6-release-bionic.deb",
-      "sudo dpkg -i puppet6-release-bionic.deb",
-      "sudo apt-get update",
-      "sudo apt-get install apache2 -y",
-      "sudo systemctl enable apache2",
-      "sudo systemctl start apache2",
-      "sudo apt-get install puppet -y",
-      "sudo chmod 777 /var/www/html/index.html",
-      "sudo apt-get install openjdk-11-jdk -y",
-      "sudo puppet apply /home/ubuntu/jenkins.pp"
-    ]
-  }       
-    
-  provisioner "file" {
-    source = "index.html"
-    destination = "/var/www/html/index.html"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo chmod 644 /var/www/html/index.html"
-    ]
-  }    
 
   # Save the public IP for testing
   provisioner "local-exec" {
-    command = "echo ${aws_instance.web_server.public_ip} > public-ip.txt"
+    command = "echo ${aws_instance.jenkins.public_ip} > upload/public-ip.txt"
   }
+
+  connection {
+    user        = "ubuntu"
+    host        = "${self.public_ip}"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+  provisioner "file" {
+    source      = "upload"
+    destination = "/home/ubuntu"
+  }
+
 }
