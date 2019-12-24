5a5f97689e02eddaad972df219123d029f1efd58
Don Perry <don.perry@dokasystems.com>
2019-12-23 18:13:10 -0600

    Create vars.tf
    variables file added

---
+++ web-server-jenkins.puppet/vars.tf
@@ -0,0 +1,12 @@
+variable server_name {
+    default = "jenkins"
+}
+variable domain_name {
+    default = "example.com."
+}
+variable ssh_key_name {
+    default = "jenkins"
+}
+variable ami {
+    default = "ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"
+}
